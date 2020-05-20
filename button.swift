e5304621c91eba2d029faa162aa00a57e0c82c1f
titanium-applewatch <65664851+titanium-applewatch@users.noreply.github.com>
2020-05-20 22:31:33 +0900

    Create button.swift

---
+++ button.swift
@@ -0,0 +1,10 @@
+Button(action: {
+    let url = URL(string: "https://twitter.com/intent/tweet?text=TEXT&url=URL&hashtags=HASHTAGS&via=twitter&related=twitter")!
+    if UIApplication.shared .canOpenURL(url) {
+        UIApplication.shared.open(url)
+    }
+    let impactMed = UIImpactFeedbackGenerator(style: .medium)
+    impactMed.impactOccurred()
+}) {
+    text("Tweet")
+}
